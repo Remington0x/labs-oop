b2c217167f9ec97c5cae75d6a8f48c601b79196a
Remington0x <alex.tervo@ya.ru>
2020-10-10 16:46:34 +0300

    vrode works

--- lab1/oop_excercise_1.cpp
+++ lab1/oop_excercise_1.cpp
@@ -2,9 +2,15 @@
 
 class Rectangle {
 public:
-    Rectangle(double, double, double, double);
+    Rectangle(double xl, double yl, double xr, double yr);
 
-    Rectangle &rec = Rectangle;
+    ~Rectangle();
+
+    //Rectangle &rec = Rectangle;
+    double xl;
+    double xr;
+    double yl;
+    double yr;
 
     double getSquare() {
         return (xr - xl) * (yr - yl);
@@ -26,94 +32,109 @@ public:
         yr += y;
     }
 
-    Rectangle &compareSquare(Rectangle &rec2) {
+    bool isSquareLarger(Rectangle &rec2) {
         double square1 = getSquare();
-        double square2 = rec2::getSquare();
-        if (square1 >= square2) {
-            return rec;
+        double square2 = rec2.getSquare();
+        if (square1 > square2) {
+            return true;
         } else {
-            return rec2;
+            return false;
         }
     }
 
-    Rectangle &comparePerimeter(Rectangle &rec2) {
+    bool isPerimeterLarger(Rectangle &rec2) {
         double perimeter1 = getPerimeter();
         double perimeter2 = rec2.getPerimeter();
-        if (perimeter1 >= perimeter2) {
-            return rec;
+        if (perimeter1 > perimeter2) {
+            return true;
         } else {
-            return rec2;
+            return false;
         }
     }
 
-    Rectangle &intersection(Rectangle &rec2) {
-        double int_x_left, int_x_right, int_y_left, int_y_right;
+    bool is_point_on(double x, double y, Rectangle &rec) {
+        if ((x >= rec.xl) && (x <= rec.xr) && (y >= rec.yl) && (y <= rec.yr)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    Rectangle intersection(Rectangle &rec2, bool &code) {
+        double int_x_left = 0;
+        double int_x_right = 0;
+        double int_y_left = 0;
+        double int_y_right = 0;
         bool a, b, c, d;
-        a = is_point_on(rec.xl, rec.yr, &rec2); //upper left
-        b = is_point_on(rec.xr, rec.yr, &rec2); //upper right
-        c = is_point_on(rec.xl, rec.yl, &rec2); //down left
-        d = is_point_on(rec.xr, rec.yl, &rec2); //down right
+        a = is_point_on(xl, yr, rec2); //upper left
+        b = is_point_on(xr, yr, rec2); //upper right
+        c = is_point_on(xl, yl, rec2); //down left
+        d = is_point_on(xr, yl, rec2); //down right
 
         if (a) {
             if (b) {    //rec2 limits only on bottom
-                int_x_left = rec.xl;
+                int_x_left = xl;
                 int_y_left = rec2.yl;
-                int_x_right = rec.xr;
-                int_y_right = rec.yr; 
+                int_x_right = xr;
+                int_y_right = yr;
             } else
             if (c) {    //rec2 limits only on right border
-                int_x_left = rec.xl;
-                int_y_left = rec.yl;
+                int_x_left = xl;
+                int_y_left = yl;
                 int_x_right = rec2.xr;
-                int_y_right = rec.yr;
+                int_y_right = yr;
             } else
             if (d) {    //that means that rec fully placed on rec2
-                int_x_left = rec.xl;
-                int_y_left = rec.yl;
-                int_x_right = rec.xr;
-                int_y_right = rec.yr;
+                int_x_left = xl;
+                int_y_left = yl;
+                int_x_right = xr;
+                int_y_right = yr;
             } else {    //that means only a placed on the rec2
-                int_x_left = rec.xl;
+                int_x_left = xl;
                 int_y_left = rec2.yl;
                 int_x_right = rec2.xr;
-                int_y_right = rec.yr;
+                int_y_right = yr;
             }
         } else
         if (b) {
             if (c) {
                 int_x_left = rec2.xl;
-                int_y_left = rec.yl;
-                int_x_right = rec.xr;
-                int_y_right = rec.yr;
+                int_y_left = yl;
+                int_x_right = xr;
+                int_y_right = yr;
             } else {    //only b
                 int_x_left = rec2.xl;
                 int_y_left = rec2.yl;
-                int_x_right = rec.xr;
-                int_y_right = rec.yr;
+                int_x_right = xr;
+                int_y_right = yr;
             }
         } else
         if (c) {
             if (d) {
-                int_x_left = rec.xl;
-                int_y_left = rec.yl;
-                int_x_right = rec.xr;
+                int_x_left = xl;
+                int_y_left = yl;
+                int_x_right = xr;
                 int_y_right = rec2.yr;
             } else {    //only c
-                int_x_left = rec.xl;
-                int_y_left = rec.yl;
+                int_x_left = xl;
+                int_y_left = yl;
                 int_x_right = rec2.xr;
                 int_y_right = rec2.yr;
             }
         } else
         if (d) {    //only d
             int_x_left = rec2.xl;
-            int_y_left = rec.yl;
-            int_x_right = rec.xr;
+            int_y_left = yl;
+            int_x_right = xr;
             int_y_right = rec2.yr;
         } else {
-            return NULL;
+            code = false;
         }
 
+        Rectangle rec_int(int_x_left, int_y_left, int_x_right, int_y_right);
+        code = true;
+
+        return rec_int;
 
     }
 };
@@ -127,5 +148,5 @@ bool is_point_on(double x, double y, Rectangle &rec) {
 }
 
 int main() {
-
+    std::cout << "Hello, world!\n";
 }
